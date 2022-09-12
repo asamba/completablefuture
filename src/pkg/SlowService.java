41ca965f37be4191a70c66ba63a566347fe84f70
anand <anand.sambamoorthy@gmail.com>
2022-09-12 14:40:55 -0500

    enhanced with executor

--- src/pkg/SlowService.java
+++ src/pkg/SlowService.java
@@ -3,9 +3,9 @@ package pkg;
 public class SlowService {
     public String callSlowExternalService(String payload) {
         try {
-            System.out.println("in slowservice : payload = " + payload);
-            Thread.sleep(1500);
-            System.out.println("Completed slowservice : payload = " + payload);
+            System.out.println("STARTING slowservice : payload = " + payload);
+            Thread.sleep(3000);
+            System.out.println("COMPLETED slowservice : payload = " + payload);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/pkg/TestMain.java
+++ src/pkg/TestMain.java
@@ -13,33 +13,38 @@ public class TestMain {
 
     public static void main(String[] args) throws Exception{
 
-        List<String> cities = Arrays.asList("Bangalore", "LA", "Chicago", "London");
+        long startTime = System.currentTimeMillis();
+
+        List<String> cities = Arrays.asList("Bangalore", "LA", "Chicago", "London", "1", "2", "3", "4", "5", "6", "7");
         SlowService slowService = new SlowService();
         List<CompletableFuture<String>> completableFutureList = new ArrayList<>();
 
         int availableProcessors = Runtime.getRuntime().availableProcessors();
         System.out.println("availableProcessors = " + availableProcessors);
-        ExecutorService yourOwnExecutor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()-2);
+        //ExecutorService yourOwnExecutor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()-2);
+        ExecutorService yourOwnExecutor = Executors.newFixedThreadPool(10);
 
         for(String city : cities){
-            CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> slowService.callSlowExternalService(city));
+            System.out.println("Calling TestMain - slowService CompletableFuture for city = " + city);
+            CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> slowService.callSlowExternalService(city), yourOwnExecutor);
             completableFutureList.add(completableFuture);
         }
 
+        //wait for all completion
         CompletableFuture.allOf(completableFutureList.toArray(new CompletableFuture[0]))
                 .exceptionally(ex -> null)
                 .join();
 
         //Map<Boolean, List<CompletableFuture<String>>> collect = completableFutureList.stream().collect(Collectors.partitioningBy(CompletableFuture::isCompletedExceptionally));
+        //print result
         List<CompletableFuture<String>> collect = completableFutureList.stream().collect(Collectors.toList());
         for (CompletableFuture<String> item : collect) {
-
             String resultStr = item.get();
             System.out.println("result = " + resultStr);
-
         }
-        System.out.println("Completed all the city processing");
 
+        long endTime = System.currentTimeMillis();
+        System.out.println("Completed all the city processing: Duration of execution "  + (endTime - startTime)/1000 + " secs");
     }
 
 }
